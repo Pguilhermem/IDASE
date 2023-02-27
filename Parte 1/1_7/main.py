903b4183d79743fd1145ffbdf417f1436167635e
Guilherme Márcio <guilherme.marcio@engenharia.ufjf.br>
2023-02-27 17:20:39 -0300

    add exemplo 1_7

---
+++ Parte 1/1_7/main.py
@@ -0,0 +1,6 @@
+from pessoas import Estudante, Trabalhador
+
+pessoas = [Estudante("João", "Engenharia"),Trabalhador("Marcelo","Apple")]
+
+for p in pessoas:
+    p.falar()
---
+++ Parte 1/1_7/pessoas.py
@@ -0,0 +1,24 @@
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def falar(self):
+        print("Olá!")
+    
+
+
+class Estudante(Pessoa):
+    def __init__(self, nome, curso):
+        super().__init__(nome)
+        self.curso = curso
+
+    def falar(self):
+        print(f"Olá, meu nome é {self.nome} e eu faço {self.curso}.")
+    
+class Trabalhador(Pessoa):
+    def __init__(self, nome, empresa):
+        super().__init__(nome)
+        self.empresa = empresa
+
+    def falar(self):
+        print(f"Olá, meu nome é {self.nome} e eu trabalho na empresa {self.empresa}.")

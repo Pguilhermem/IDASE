ad65f4408162a4800631e3b46455cfb44235b5e6
Guilherme Márcio <guilherme.marcio@engenharia.ufjf.br>
2023-03-01 00:51:03 -0300

    add docstrings to 1_11

--- Parte 1/1_11/processamento_imagens.py
+++ Parte 1/1_11/processamento_imagens.py
@@ -3,18 +3,45 @@ import multiprocessing
 import cv2
 
 class ProcessamentoImagens:
-    def __init__(self, diretorio_entrada, diretorio_saida, num_processos=4):
+    """
+    Classe responsável por processar imagens em paralelo usando múltiplos processos.
+
+    Atributos:
+    ----------
+    diretorio_saida : str
+        Caminho para o diretório onde as imagens processadas serão salvas.
+    diretorio_entrada : str
+        Caminho para o diretório onde as imagens a serem processadas estão localizadas.
+    num_processos : int
+        Número de processos que serão criados para processar as imagens.
+    arquivos : list
+        Lista de arquivos encontrados no diretório de entrada.
+    fila : multiprocessing.Queue
+        Fila que armazena os lotes de arquivos a serem processados.
+    infos : dict
+        Dicionário que armazena informações sobre o processamento, como o número de imagens processadas e erros ocorridos.
+    """
+    def __init__(self, diretorio_entrada:str, diretorio_saida:str, num_processos:int=4) -> None:
+        """
+        Inicializa a classe ProcessamentoImagens.
+
+        :param diretorio_entrada: Caminho para o diretório onde as imagens a serem processadas estão localizadas.
+        :param diretorio_saida: Caminho para o diretório onde as imagens processadas serão salvas.
+        :param num_processos: Número de processos que serão criados para processar as imagens. (padrão: 4)
+        """
         self.diretorio_saida = diretorio_saida
         self.diretorio_entrada = diretorio_entrada
-         # Lista os arquivos do diretório de entrada
         self.arquivos = [f for f in os.listdir(self.diretorio_entrada)]
-
         self.num_processos = num_processos
-        #Cria a fila
         self.fila = multiprocessing.Queue()
         self.infos = {"num_imgs":len(self.arquivos),"erros":[]}
 
-    def processa_imagem(self, path_img): #Realiza a detecção de faces com um classificador pré-treinado
+    def processa_imagem(self, path_img:str) -> None:
+        """
+        Realiza a detecção de faces em uma imagem usando um classificador pré-treinado e salva a imagem com as faces detectadas no diretório de saída.
+
+        :param path_img: Caminho para a imagem a ser processada.
+        """
         try:
             imagem = cv2.imread(os.path.join(self.diretorio_entrada, path_img))
             classificador = cv2.CascadeClassifier("multiprocessing_env\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml")
@@ -30,27 +57,33 @@ class ProcessamentoImagens:
         except Exception as e:
             self.infos["erros"].append({path_img:e.args})
 
-    def processa_lote(self, fila):
+    def processa_lote(self, fila:multiprocessing.Queue) -> None:
+        """
+        Processa um lote de imagens da fila.
+
+        :param fila: Fila que armazena os lotes de arquivos a serem processados.
+        """
         lote = fila.get()
         for img in lote:
             self.processa_imagem(img)
 
     def inicia_processamento(self):
-        # Divide a lista de arquivos em lotes e coloca na fila
+        """
+        Inicia o processamento das imagens em paralelo usando múltiplos processos.
+
+        :return: Dicionário que armazena informações sobre o processamento, como o número de imagens processadas e erros ocorridos.
+        """
         tamanho_lote = int(len(self.arquivos) / self.num_processos) + 1
         for i in range(0, len(self.arquivos), tamanho_lote):
             self.fila.put(self.arquivos[i:i+tamanho_lote])
         
-        # Inicia os processos
         processos = []
         for i in range(self.num_processos):
             p = multiprocessing.Process(target=self.processa_lote, args=(self.fila,))
             p.start()
             processos.append(p)
 
-        # Finaliza os processos
         for p in processos:
             p.join()
 
-        # Retorna o arquivo com informações do processamento
         return self.infos

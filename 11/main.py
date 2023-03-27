878352151ec00277b0f32d0aface08ed6a19abc2
Guilherme <guilherme.marcio@engenharia.ufjf.br>
2023-03-27 19:27:33 -0300

    update 11

--- 11/main.py
+++ 11/main.py
@@ -1,18 +1,18 @@
 import asyncio
 
 
-async def count():
+async def tarefa():
     print("Um")
     await asyncio.sleep(1)
     print("Dois")
 
 
 async def main():
-    await asyncio.gather(count(), count(), count())
+    await asyncio.gather(tarefa(), tarefa(), tarefa())
 
 if __name__ == "__main__":
     import time
     tempo_inicial = time.perf_counter()
     asyncio.run(main())
     tempo_gasto = time.perf_counter() - tempo_inicial
-    print(f"{__file__} executado em {tempo_gasto:0.2f} segundos.")
+    print(f"Programa executado em {tempo_gasto:0.2f} segundos.")

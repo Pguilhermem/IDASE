c4f9d5d786df41406fb4ef5779dd3b3b67a93d4e
Guilherme <guilherme.marcio@engenharia.ufjf.br>
2023-04-06 18:23:24 -0300

    add 22

---
+++ 22/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('parametros_coleta_imagens.csv')
+
+print("============== Estatísticas Descritivas do DataFrame ===============\r\n", df.describe())
+
+print("============== Matriz de Correlação ===============\r\n", df.corr())
+
+mean = df['Qualidade da Imagem'].mean()
+std = df['Qualidade da Imagem'].std()
+
+outliers = (df['Qualidade da Imagem'] > mean + std) | (df['Qualidade da Imagem'] < mean - std)
+n_outliers = outliers.sum()
+
+print("============== Quantidade de amostras com valores fora da média +- desvio padrão ===============\r\n", n_outliers)

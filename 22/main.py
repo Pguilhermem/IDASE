bd9826829e10f6c4c9eca9d24c42d488800ae9bf
Guilherme <guilherme.marcio@engenharia.ufjf.br>
2023-04-15 00:19:33 -0300

    add 25

--- 22/main.py
+++ 22/main.py
@@ -9,7 +9,8 @@ print("============== Matriz de Correlação ===============\r\n", df.corr())
 mean = df['Qualidade da Imagem'].mean()
 std = df['Qualidade da Imagem'].std()
 
-outliers = (df['Qualidade da Imagem'] > mean + std) | (df['Qualidade da Imagem'] < mean - std)
+outliers = (df['Qualidade da Imagem'] > mean + 2 *
+            std) | (df['Qualidade da Imagem'] < mean - 2*std)
 n_outliers = outliers.sum()
 
 print("============== Quantidade de amostras com valores fora da média +- desvio padrão ===============\r\n", n_outliers)

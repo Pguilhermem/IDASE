9d015c4d774c509b4d5043331ca8018c38a32d78
Guilherme <guilherme.marcio@engenharia.ufjf.br>
2023-05-01 20:38:12 -0300

    conformidade linter

--- 27/main.py
+++ 27/main.py
@@ -1,17 +1,23 @@
-from mongodbhandler import MongoDBHandler
+"""
+Arquivo exemplo do uso do módulo mongodbhandler
+Autor: prof. Guilherme Márcio Soares
+Instituição: UFJF
+"""
 from datetime import datetime
 from bson.objectid import ObjectId
+from mongodbhandler import MongoDBHandler
+
 
 db_handler = MongoDBHandler("mongodb://localhost:27017/")
 
 # Inserção de documentos na coleção
 data = datetime.now()
 doc1 = {"ts": data, "temperatura": 400.0,
-"pressao": 36450.0, "umidade": 33.0, "consumo": 62.0}
+        "pressao": 36450.0, "umidade": 33.0, "consumo": 62.0}
 doc2 = {"ts": data, "temperatura": 401.0,
-"pressao": 35635.0, "umidade": 32.0, "consumo": 65.0}
+        "pressao": 35635.0, "umidade": 32.0, "consumo": 65.0}
 doc3 = {"ts": data, "temperatura": 402.0,
-"pressao": 38965.0, "umidade": 31.0, "consumo": 78.0}
+        "pressao": 38965.0, "umidade": 31.0, "consumo": 78.0}
 db_handler.insert_data([doc1, doc2, doc3])
 
 # Busca de documentos com filtro de data e hora
@@ -24,6 +30,7 @@ db_handler.update_data(filtro, novo_valor)
 db_handler.search_data("23/06/2022 10:22:45", "23/06/2022 10:25:53")
 
 # Remoção de um documento
-query = {"ts": datetime.strptime("23/06/2022 13:22:50.903", "%d/%m/%Y %H:%M:%S.%f")}
+query = {"ts": datetime.strptime(
+    "23/06/2022 13:22:50.903", "%d/%m/%Y %H:%M:%S.%f")}
 db_handler.delete_data(query)
 db_handler.search_data("23/06/2022 10:22:45", "23/06/2022 10:22:53")
--- 27/mongodbhandler.py
+++ 27/mongodbhandler.py
@@ -1,23 +1,49 @@
+"""
+Módulo mongodbhandler
+Autor: prof. Guilherme Márcio Soares
+Instituição: UFJF
+"""
 from datetime import datetime
 import pymongo
 import pytz
 
 
 class MongoDBHandler:
+    """
+    Classe Mongo_DBHandler
+    """
+
     def __init__(self, database_url):
+        """
+        Inicializa uma conexão com o banco de dados MongoDB.
+
+        :param database_url: URL de conexão com o banco de dados.
+        """
         self.client = pymongo.MongoClient(database_url)
-        self.db = self.client["database"]
-        self.collection = self.db["data"]
+        self._db = self.client["database"]
+        self.collection = self._db["data"]
 
     def insert_data(self, data):
+        """
+        Insere dados na coleção 'data' do banco de dados.
+
+        :param data: lista de dicionários com os valores a serem inseridos na coleção.
+        """
         self.collection.insert_many(data)
 
     def search_data(self, inicio, fim):
+        """
+        Realiza uma busca na coleção 'data' do banco de dados.
+
+        :param inicio: string contendo a data/hora de início da busca no formato 'dd/mm/yyyy HH:MM:SS'.
+        :param fim: string contendo a data/hora de término da busca no formato 'dd/mm/yyyy HH:MM:SS'.
+        """
         local_tz = pytz.timezone('America/Sao_Paulo')
         utc_tz = pytz.timezone('UTC')
         inicio_local = local_tz.localize(
             datetime.strptime(inicio, '%d/%m/%Y %H:%M:%S'))
-        fim_local = local_tz.localize(datetime.strptime(fim, '%d/%m/%Y %H:%M:%S'))
+        fim_local = local_tz.localize(
+            datetime.strptime(fim, '%d/%m/%Y %H:%M:%S'))
         inicio_utc = inicio_local.astimezone(utc_tz)
         fim_utc = fim_local.astimezone(utc_tz)
         query = {"ts": {"$gte": inicio_utc, "$lte": fim_utc}}
@@ -30,7 +56,18 @@ class MongoDBHandler:
                 f"_id: {doc['_id']}| Timestamp:{hora_local} | Temperatura: {doc['temperatura']}")
 
     def update_data(self, filtro, novo_valor):
+        """
+        Atualiza um registro na coleção 'data' do banco de dados.
+
+        :param filtro: dicionário contendo o filtro para identificar o registro a ser atualizado.
+        :param novo_valor: dicionário contendo os novos valores a serem atribuídos ao registro.
+        """
         self.collection.update_one(filtro, novo_valor)
 
     def delete_data(self, query):
+        """
+        Remove um registro da coleção 'data' do banco de dados.
+
+        :param query: dicionário contendo o filtro para identificar o registro a ser removido.
+        """
         self.collection.delete_one(query)
